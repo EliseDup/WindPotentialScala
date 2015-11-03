974e30a628d49f7ad3001c8fe465972673571acd
EliseDup <Elise@wifi-secure1-593.sri.ucl.ac.be>
2015-11-03 10:58:49 +0100

    changes

--- src/main/scala/download/Launcher.scala
+++ src/main/scala/download/Launcher.scala
@@ -1,17 +1,17 @@
 package download
 
 object Launcher {
-  
+
   def main(args: Array[String]) = {
     val wind = new WindData
     val solar = new SolarData
     println("Wind : " + wind.meanFactor + " (" + wind.n + "observations)")
     println("Solar : " + solar.meanFactor + " (" + solar.n + "observations)")
-    
-    val helper = new PlotHelper
-helper.plotTime(wind.times, wind.data.map(_.capacity), "Actuals", "")
-    //helper.plotXY(wind.windData.map(_.capacityFactor), "Capacity Factor",  wind.windData.map(_.capacity), "Total installed", "")
- helper.histogram(wind.data.map(_.capacityFactor), 100, "")
-  }
 
+    val helper = new PlotHelper
+    helper.plotTime(wind.times, wind.data.map(_.capacity), "", "")
+    //helper.plotXY(wind.windData.map(_.capacityFactor), "Capacity Factor",  wind.windData.map(_.capacity), "Total installed", "")
+    //helper.histogram(wind.data.map(_.capacityFactor), 100, "")
+  }
+  
 }
