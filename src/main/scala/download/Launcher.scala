19ac682b3c154736614e67a243aa7e83687d965c
EliseDup <Elise@wifi-secure1-593.sri.ucl.ac.be>
2015-11-03 09:58:58 +0100

    changes

--- src/main/scala/download/Launcher.scala
+++ src/main/scala/download/Launcher.scala
@@ -1,11 +1,14 @@
 package download
 
-import scalax.chart.api._
-
 object Launcher {
+  
   def main(args: Array[String]) = {
     val wind = new WindData
     val meanFactor = wind.windData.map(_.capacityFactor).sum / wind.n
     println(meanFactor + " (" + wind.n + "observations)")
+     val helper = new PlotHelper
+   //  helper.plotTime(wind.times, wind.windData.map(_.capacity), "Actuals", "")
+    helper.plotXY(wind.windData.map(_.capacityFactor), "Capacity Factor",  wind.windData.map(_.capacity), "Total installed", "")
   }
+
 }
