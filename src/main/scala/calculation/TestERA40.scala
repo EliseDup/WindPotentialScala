f4544ba65a64855b0f1a708c788e0d791ef5ffca
EliseDup <Elise@wifi-secure1-386.sri.ucl.ac.be>
2015-12-16 16:29:30 +0100

    Progress

--- src/main/scala/calculation/TestERA40.scala
+++ src/main/scala/calculation/TestERA40.scala
@@ -9,13 +9,13 @@ import utils.GeoPoint
 
 object TestERA40 {
   def main(args: Array[String]) = {
-    // Helper.txtToCSV(Helper.ressourcesPy + "results/meanWindLCEurope", Helper.ressourcesPy + "windEurope.csv")
+    //Helper.txtToCSV(Helper.ressourcesPy + "results/meanWindLCEurope", Helper.ressourcesPy + "windEurope.csv", List(4,3,2),true)
 
-    val wind = new GridData
-    println("Grid Size : " + wind.grids.size + "=" + wind.grids.map(_.cellSize).sum + " km2")
+    val wind = new GridData("results/europeDailyLC2002")
+    println("Grid Size : " + wind.grids.size + "=" + wind.grids.map(_.area).sum + " km2")
 
-    //PlotHelper.cumulativeDensity(List((wind.windSpeeds, "10 metres"), (wind.windSpeeds80, "80 metres")), xLabel = "% of Sites", yLabel = "Mean Wind Speed [m/s]")
-    //PlotHelper.cumulativeDensity(List((wind.windSpeedsLand, "10 metres"), (wind.windSpeedsLand80, "80 metres")), xLabel = "% of Sites", yLabel = "Mean Wind Speed [m/s]", title = "Water Bodies Excluded")
+    PlotHelper.cumulativeDensity(List((wind.windSpeeds, "10 metres"), (wind.windSpeeds80, "80 metres")), xLabel = "% of Sites", yLabel = "Mean Wind Speed [m/s]")
+    PlotHelper.cumulativeDensity(List((wind.windSpeedsLand, "10 metres"), (wind.windSpeedsLand80, "80 metres")), xLabel = "% of Sites", yLabel = "Mean Wind Speed [m/s]")
     // PlotHelper.cumulativeDensity(wind.grids.map(_.powerDensity(80)), 100, "80 meters mean speed")
 
   }
