4a6b51a31532f0f3dca2c5e18c845b003538f618
EliseDup <Elise@wifi-secure1-739.sri.ucl.ac.be>
2016-01-13 16:44:12 +0100

    progress on small GUI

--- src/main/scala/embodiedEnergy/Materials.scala
+++ src/main/scala/embodiedEnergy/Materials.scala
@@ -5,62 +5,52 @@ import squants.mass._
 import squants.mass.MassConversions
 import org.joda.convert.ToString
 import calculation._
+import squants.time.Hours
+import utils._
 
-object Test {
-  def main(args: Array[String]): Unit = {
-    val a = new WindTurbine2MW
-   a.plot
-    val b = new WindTurbine3MW
-    b.plot
-    val c = new WindTurbine850kW
-   // c.plot
+class Materials(val list: List[Material]) {
+  def apply(name: String): Option[Material] = list.find(_.name.equals(name))
+}
+
+case class Material(name: String, energy: Energy, unit: Mass) {
+  val energyIntensity = energy / unit
+  def toArray = List(name,energy.value,energy.unit.symbol,unit.value,unit.unit.symbol).toArray
+  }
+
+/**
+ * A list of materials used in wind turbine manufacturing and their
+ * energy efficiency [GJ/unit]
+ */
+object Materials {
+  def apply() = {
+    new Materials(List(
+      Material("Concrete", Gigajoules(3.6), Tonnes(1)),
+      Material("Steel", Gigajoules(85.3), Tonnes(1)),
+      Material("Aluminium", Gigajoules(252), Tonnes(1)),
+      Material("Copper", Gigajoules(379), Tonnes(1)),
+      Material("GlassFibre", Gigajoules(168), Tonnes(1)),
+      Material("EpoxyResin", Gigajoules(163), Tonnes(1)),
+      Material("Iron", Gigajoules(0.096), Kilograms(10)),
+      Material("Silica", Gigajoules(0), Tonnes(1))))
   }
 }
+/*object Concrete extends Material(Megajoules(6.03E3), Kilograms(2400))
+object Steel extends Material(Megajoules(3.26E4), Tonnes(1))
+object Aluminium extends Material(Megajoules(2.08E5), Tonnes(1)) 
+object Copper extends Material(Megajoules(1.64E5), Tonnes(1))
+object GlassFibre extends Material(Megajoules(1.88E5), Tonnes(1))
+object EpoxyResin extends Material(Megajoules(4.57E4), Tonnes(1))
+object Paint extends Material(Megajoules(0), Kilograms(1))
+object Iron extends Material(Megajoules(0), Tonnes(1))
+object Silica extends Material(Megajoules(3.06E4), Tonnes(1))*/
 
-case class Material(val energy: Energy, val unit: Mass) {
-  val energyIntensity = energy / unit
-}
-
-object Concrete20MPa extends Material(Gigajoules(4.08), Kilograms(2400))
+/*object Concrete extends Material(Gigajoules(4.08), Kilograms(2400))
 object Steel extends Material(Gigajoules(85.3), Tonnes(1))
-object Aluminium extends Material(Gigajoules(252), Tonnes(1)) //54 MJ / kg average worldwide?
-object Copper extends Material(Gigajoules(379), Tonnes(1)) // 25 MJ / kg average worldwide?
+object Aluminium extends Material(Gigajoules(252), Tonnes(1)) 
+object Copper extends Material(Gigajoules(379), Tonnes(1))
 object GlassFibre extends Material(Gigajoules(168), Tonnes(1))
-object Epoxy extends Material(Gigajoules(163), Tonnes(1))
-object Paint extends Material(Gigajoules(0.096), Kilograms(10)) // 1m^2 ?
+object EpoxyResin extends Material(Gigajoules(163), Tonnes(1))
+object Paint extends Material(Gigajoules(0.096), Kilograms(10))
+object Iron extends Material(Gigajoules(0), Tonnes(1))
+object Silica extends Material(Gigajoules(1), Tonnes(1))*/
 
-class WindTurbineComponent(val materials: List[(Mass, Material)] = List()) {
-  val weight = materials.map(_._1).foldLeft(Tonnes(0))(_ + _)
-  val embodiedEnergy = materials.map(m => m._2.energyIntensity * m._1).foldLeft(Gigajoules(0))(_ + _)
-  val energyIntensity = embodiedEnergy / weight
-}
-object WindTurbineComponent {
-  def apply(values: (Double, Material)*) = new WindTurbineComponent(values.map(v => (Tonnes(v._1), v._2)).toList)
-}
-
-class WindTurbineComponents(val ratedPower: Power,
-  val foundation: WindTurbineComponent = new WindTurbineComponent(),
-  val tower: WindTurbineComponent = new WindTurbineComponent(),
-  val nacelle: WindTurbineComponent = new WindTurbineComponent(),
-  val rotor: WindTurbineComponent = new WindTurbineComponent())
-    extends WindTurbineComponent(foundation.materials ++ tower.materials ++ nacelle.materials ++ rotor.materials) {
-  override def toString = "Wind turbine power : " + ratedPower + ", total weight : " + weight + ",total energy embodied : " + embodiedEnergy + ", =>" + energyIntensity
-}
-
-class WindTurbine850kWComponents extends WindTurbineComponents(Kilowatts(850),
-  foundation = WindTurbineComponent((480.0, Concrete20MPa), (15.0, Steel)),
-  tower = WindTurbineComponent((69.07, Steel), (0.93, Paint)),
-  nacelle = WindTurbineComponent((3.35 + 2.41 + 1.47 + 4.21 + 0.26 + 0.26 + 6.08 + 1 + 0.26 + 0.894, Steel), (0.37 + 0.062 + 0.24 + 0.357, Copper), (0.062 + 0.18 + 0.357, Aluminium)), //(0.18,Plastic)
-  rotor = WindTurbineComponent((4.8, Steel), (3.01, GlassFibre), (2.01, Epoxy), (0.18, Steel)))
-// TODO
-class WindTurbine2MWComponents extends WindTurbineComponents(Megawatts(2),
-  foundation = WindTurbineComponent((1140.0, Concrete20MPa), (36.0, Steel)),
-  tower = WindTurbineComponent((158.76, Steel), (1.24, Paint)),
-  nacelle = WindTurbineComponent((13 + 9.33 + 5.71 + 1.02 + 23.58 + 3.87 + 1.02 + 3.47, Steel), (1.43 + 0.241 + 0.94 + 1.38, Copper), (0.241 + 0.69 + 1.38, Aluminium)), //(0.7,Plastic)
-  rotor = WindTurbineComponent((19.2, Steel), (12.04, GlassFibre), (8.03, Epoxy), (0.73, Steel)))
-
-class WindTurbine3MWComponents extends WindTurbineComponents(Megawatts(3),
-  foundation = WindTurbineComponent((1140.0, Concrete20MPa), (36.0, Steel)),
-  tower = WindTurbineComponent((158.76, Steel), (1.24, Paint)),
-  nacelle = WindTurbineComponent((13 + 9.33 + 5.71 + 1.02 + 23.58 + 3.87 + 1.02 + 3.47, Steel), (1.43 + 0.241 + 0.94 + 1.38, Copper), (0.241 + 0.69 + 1.38, Aluminium)), //(0.7,Plastic)
-  rotor = WindTurbineComponent((19.2, Steel), (12.04, GlassFibre), (8.03, Epoxy), (0.73, Steel)))
--- src/main/scala/utils/Units.scala
+++ src/main/scala/utils/Units.scala
@@ -6,10 +6,23 @@ import squants.energy.Watts
 import squants.MetricSystem
 import squants.energy.SpecificEnergyUnit
 import squants.mass._
+import squants.energy._
 
 object TerawattHours extends EnergyUnit {
-  val conversionFactor = Watts.conversionFactor * MetricSystem.Tera
+  val conversionFactor = Watts.conversionFactor  * MetricSystem.Tera
   val symbol = "TWh"
+}
+object TonOilEquivalent extends EnergyUnit {
+  val conversionFactor = 11.63 * Watts.conversionFactor  * MetricSystem.Mega
+  val symbol = "toe"
+}
+object MegaTonOilEquivalent extends EnergyUnit {
+  val conversionFactor = TonOilEquivalent.conversionFactor * MetricSystem.Mega 
+  val symbol = "Mtoe"
+}
+object Terawatts extends PowerUnit {
+  val conversionFactor = MetricSystem.Tera
+  val symbol = "TW"
 }
 // Gray is J/kg
 object GigajoulesPerton extends SpecificEnergyUnit {
