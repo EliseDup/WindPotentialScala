4eb924b4a81a32e524b34945e323b1ea8700edeb
EliseDup <Elise@wifi-secure1-739.sri.ucl.ac.be>
2016-01-13 17:10:40 +0100

    gui

--- src/main/scala/GUI/GUI.scala
+++ src/main/scala/GUI/GUI.scala
@@ -9,41 +9,37 @@ import energyGeneration.GridData
 import utils.PlotHelper
 import squants.space.Degrees
 import squants.space.SquareKilometers
-import squants.energy.Kilowatts
-import squants.energy.Megawatts
+import squants.energy._
 import calculation.WindTurbine
 
 object GUI extends SimpleSwingApplication {
-
-  // Simulation tab 
-  lazy val mainUI = new BoxPanel(Orientation.Vertical) {
-    val area = new ComboBox(List("Europe", "World"))
-    val sizeTurbine = List(Kilowatts(850), Megawatts(2), Megawatts(3))
-    val turbine = new ComboBox(List("850 kW", "2 MW", "3 MW"))
-
-    val start = new Button {
-      text = "Go !"
-    }
-
-    listenTo(start)
-    reactions += {
-      case ButtonClicked(start) =>
-        val wind = new GridData("results/" + area.selection.item.toLowerCase() + "5yearslc", Degrees(0.25), WindTurbine(sizeTurbine(turbine.selection.index)), WindTurbine(sizeTurbine(turbine.selection.index)))
-        wind.plotEROIVSCumulatedProduction(wind.agriculturalAreas(wind.clcGrids))
-        PlotHelper.cumulativeDensity(List((wind.erois(wind.agriculturalAreas(wind.clcGrids)), "")), xLabel = "% of Sites", yLabel = "EROI")
-
-        println(area.selection.item + "\t" + turbine.selection.item)
-    }
-
-    contents += new FlowPanel(new Label("Area Studied"), area); contents += new FlowPanel(new Label("Wind Turbine Rated Power"), turbine); contents += start;
-  }
-
+  
+  val sizeTurbine = List(Kilowatts(850), Megawatts(2), Megawatts(3))
+  
   def top = new MainFrame {
     title = "Wind power potential"
-    preferredSize = new Dimension(500, 350)
-    contents = new TabbedPane {
-      pages += new Page("Simulation", mainUI)
-      pages += new Page("Materials", new MaterialsPage)
+    preferredSize = new Dimension(750, 500)
+    val materials = new MaterialsTab
+    val simu = new SimulationTab(sizeTurbine)
+    val start = new Button { text = "Go !" }
+
+    contents = new FlowPanel {
+      contents.append(new TabbedPane {
+        pages += new Page("Simulation", simu)
+        pages += new Page("Materials", materials)
+      })
+      contents.append(start)
+    }
+    listenTo(start)
+
+    reactions += {
+      case ButtonClicked(start) =>
+        val wind = new GridData("results/" +
+          simu.area.selection.item.toLowerCase() + "5yearslc", Degrees(0.25),
+          WindTurbine(sizeTurbine(simu.onshore.selection.index)), WindTurbine(sizeTurbine(simu.offshore.selection.index)))
+        
+        wind.plotEROIVSCumulatedProduction(wind.agriculturalAreas(wind.clcGrids))
+        PlotHelper.cumulativeDensity(List((wind.erois(wind.agriculturalAreas(wind.clcGrids)), "")), xLabel = "% of Sites", yLabel = "EROI")
     }
   }
 
