3d9e7fe2aed4586d3c7e428063f6c31422f00e9e
EliseDup <Elise@wifi-secure1-175.sri.ucl.ac.be>
2016-01-15 17:45:08 +0100

    progress

--- src/main/scala/GUI/TestERA40.scala
+++ src/main/scala/GUI/TestERA40.scala
@@ -15,14 +15,14 @@ import construction._
 object TestERA40 {
   def main(args: Array[String]) = {
     //Helper.txtToCSV(Helper.ressourcesPy + "results/worldDailyAugust2002test", Helper.ressourcesPy + "test.csv", List(4,3,2),true)
-    val name = "europe5years"
+    val name = "world5years"
     val wind = new GridData("results/" + name, Degrees(0.25), new WindTurbine2MW, new WindTurbine2MW)
 
     println(name + "\t" + "Grid Size : " + wind.grids.size + "=" + wind.grids.map(_.area).foldLeft(SquareKilometers(0))(_ plus _))
-    wind.plotEROIVSCumulatedProduction(wind.agriculturalAreas(wind.clcGrids))
-    // PlotHelper.cumulativeDensity(List((wind.windSpeeds(), "10 metres"), (wind.windSpeeds80(), "80 metres")), xLabel = "% of Sites", yLabel = "Mean Wind Speed [m/s]")
+    wind.plotEROIVSCumulatedProduction(wind.landGrids())
+    PlotHelper.cumulativeDensity(List((wind.windSpeeds(), "10 metres"), (wind.windSpeeds(atHub=true), "80-120 metres")), xLabel = "% of Sites", yLabel = "Mean Wind Speed [m/s]")
     // PlotHelper.cumulativeDensity(List((wind.powerDensities(), "10 metres"), (wind.powerDensities80(), "80 metres")), xLabel = "% of Sites", yLabel = "Power Density [W/m^2]")
-    PlotHelper.cumulativeDensity(List((wind.erois(wind.agriculturalAreas(wind.clcGrids)), "")), xLabel = "% of Sites", yLabel = "EROI")
+    PlotHelper.cumulativeDensity(List((wind.erois(wind.landGrids()), "")), xLabel = "% of Sites", yLabel = "EROI")
 
   }
 }
--- src/main/scala/construction/Component.scala
+++ src/main/scala/construction/Component.scala
@@ -4,48 +4,65 @@ import squants.energy._
 import squants.mass._
 import squants.time.Hours
 import utils._
+
 /**
  * No matter how we calculate it, we just want to have the total weigth, and embodied energy !
  */
-abstract class AbstractWindTurbineComponents {
-  val weight : Mass
-  val embodiedEnergy : Energy
-  def energyIntensity : SpecificEnergy = embodiedEnergy / weight
-  
-  override def toString = "Wind turbine : total weight : " + weight + ",total energy embodied : " + embodiedEnergy + ", =>" + energyIntensity.to(GigajoulesPerton) + "GJ/t"
+abstract class Components {
+  val components: List[(Mass, Material)]
 
+  def weight = components.map(_._1).foldLeft(Tonnes(0))(_ + _)
+  def embodiedEnergy: Energy = components.map(c => c._2.energyIntensity * c._1).foldLeft(Gigajoules(0))(_ + _)
+  def energyIntensity: SpecificEnergy = embodiedEnergy / weight
+
+  override def toString = "Wind turbine : total weight : " + weight + ",total energy embodied : " + embodiedEnergy + ", =>" + energyIntensity.to(GigajoulesPerton) + "GJ/t"
 }
+
+object Components {
+  def apply(file: String, startRow: Int, endRow: Int): Components = {
+    val sheet = Helper.xlsSheet(file, 0)
+    new Components {
+      val components = (startRow to endRow).map(r => {
+        val row = sheet.getRow(r)
+        val mass = Helper.toDouble(row, 1)
+        val intensity = Helper.toDouble(row, 3)
+        (Kilograms(mass), Material(Helper.toString(row, 0), Megajoules(1.0 / intensity), Kilograms(1)))
+      }).toList
+    }
+  }
+}
+
 /**
  * 3MW : 1443.302 tons
  * 2MW : 962.844 tons
  * 850 kW : 596.822 tons
- * 
+ *
  * From "Energy and CO2 life-cycle analyses of wind turbines- review and applications
  */
-class DefaultWindTurbineComponents(val weight: Mass)  extends AbstractWindTurbineComponents {
-  val blades = (weight*0.027, Material("Glass fibre, epoxy,PVC", Megajoules(61.8), Kilograms(1)))
-  val hub = (weight*0.035, Material("Steel", Megajoules(36.8), Kilograms(1)))
-  val transmission = (weight*0.052, Material("Steel", Megajoules(36.8), Kilograms(1)))
-  val generator = (weight*0.026, Material("Copper", Megajoules(86.2), Kilograms(1)))
-  val nacelle = (weight*0.003, Material("Glass fibre", Megajoules(61.8), Kilograms(1)))
-  val tower = (weight*0.233, Material("Steel", Megajoules(36.8), Kilograms(1)))
-  val foundations = (weight*0.603, Material("Concrete", Megajoules(3.2), Kilograms(1)))
-  val electrical = (weight*0.021, Material("Copper", Megajoules(86.2), Kilograms(1)))
-  
-  val components = List(blades,hub,transmission,generator,nacelle,tower,foundations,electrical)
-  val embodiedEnergy = components.map(c => c._2.energyIntensity*c._1).foldLeft(Gigajoules(0))(_ + _)
+class DefaultWindTurbineComponents(val totWeight: Mass) extends Components {
+
+  val blades = (totWeight * 0.027, Material("Glass fibre, epoxy,PVC", Megajoules(61.8), Kilograms(1)))
+  val hub = (totWeight * 0.035, Material("Steel", Megajoules(36.8), Kilograms(1)))
+  val transmission = (totWeight * 0.052, Material("Steel", Megajoules(36.8), Kilograms(1)))
+  val generator = (totWeight * 0.026, Material("Copper", Megajoules(86.2), Kilograms(1)))
+  val nacelle = (totWeight * 0.003, Material("Glass fibre", Megajoules(61.8), Kilograms(1)))
+  val tower = (totWeight * 0.233, Material("Steel", Megajoules(36.8), Kilograms(1)))
+  val foundations = (totWeight * 0.603, Material("Concrete", Megajoules(3.2), Kilograms(1)))
+  val electrical = (totWeight * 0.021, Material("Copper", Megajoules(86.2), Kilograms(1)))
+
+  val components = List(blades, hub, transmission, generator, nacelle, tower, foundations, electrical)
+
 }
 
 /**
  * To get the embodied energy in wind turbine due to the manufacturing of the components
  */
-class WindTurbineComponents(val ratedPower: Power,
-  val foundation: WindTurbineComponent,
-  val tower: WindTurbineComponent,
-  val nacelle: WindTurbineComponent,
-  val rotor: WindTurbineComponent)
-
-    extends WindTurbineComponent(foundation.components ++ tower.components ++ nacelle.components ++ rotor.components) {
+class WindTurbineComponents(
+    val foundation: Components,
+    val tower: Components,
+    val nacelle: Components,
+    val rotor: Components) extends Components {
+  val components = (foundation.components ++ tower.components ++ nacelle.components ++ rotor.components)
 }
 
 object WindTurbineComponents {
@@ -59,32 +76,32 @@ object WindTurbineComponents {
   }
 }
 
-class WindTurbineComponent(val components: List[(Mass, String)] = List(), val materials: Materials = Materials()) extends AbstractWindTurbineComponents {
-  val weight = components.map(_._1).foldLeft(Tonnes(0))(_ + _)
-  val embodiedEnergy = components.map(m => materials(m._2).getOrElse(Material("", Joules(0), Tonnes(1))).energyIntensity * m._1).foldLeft(Gigajoules(0))(_ + _)
-}
-
-object WindTurbineComponent {
-  def apply(values: (Double, String)*) = new WindTurbineComponent(values.map(v => (Tonnes(v._1), v._2)).toList)
-  }
 /**
  *
  * Examples of the composition of actual wind turbines
  *
  */
-class WindTurbine850kWComponents extends WindTurbineComponents(Kilowatts(850),
+class WindTurbineComponent(comp: List[(Double, String)]) extends Components {
+  val materials = Materials()
+  val components = comp.map(c => (Tonnes(c._1), materials(c._2).get))
+}
+object WindTurbineComponent {
+  def apply(list: (Double, String)*) = new WindTurbineComponent(list.toList)
+}
+
+class WindTurbine850kWComponents extends WindTurbineComponents(
   foundation = WindTurbineComponent((480.0, "Concrete"), (15.0, "Steel")),
   tower = WindTurbineComponent((69.07, "Steel"), (0.93, "Paint")),
   nacelle = WindTurbineComponent((3.35 + 2.41 + 1.47 + 4.21 + 0.26 + 0.26 + 6.08 + 1 + 0.26 + 0.894, "Steel"), (0.37 + 0.062 + 0.24 + 0.357, "Copper"), (0.062 + 0.18 + 0.357, "Aluminium")), //(0.18,Plastic)
   rotor = WindTurbineComponent((4.8, "Steel"), (3.01, "GlassFibre"), (2.01, "EpoxyResin"), (0.18, "Steel")))
 
-class WindTurbine2MWComponents extends WindTurbineComponents(Megawatts(2),
+class WindTurbine2MWComponents extends WindTurbineComponents(
   foundation = WindTurbineComponent((700, "Concrete"), (25, "Iron"), (15, "Steel")),
   tower = WindTurbineComponent((143, "Steel")),
   nacelle = WindTurbineComponent((6.1 + 3.3 + 4.29 + 8, "Steel"), (1.5 + 2, "Copper"), (0.149 + 0.195, "Silica"), (10.5 + 8, "Iron"), (0.8, "GlassFibre"), (1.2, "EpoxyResin")),
   rotor = WindTurbineComponent((7.8 + 0.124, "GlassFibre"), (11.7 + 0.186, "EpoxyResin"), (14, "Iron")))
 
-class WindTurbine3MWComponents extends WindTurbineComponents(Megawatts(3),
+class WindTurbine3MWComponents extends WindTurbineComponents(
   foundation = WindTurbineComponent((1140.0, "Concrete"), (36.0, "Steel")),
   tower = WindTurbineComponent((158.76, "Steel"), (1.24, "Paint")),
   nacelle = WindTurbineComponent((13 + 9.33 + 5.71 + 1.02 + 23.58 + 3.87 + 1.02 + 3.47, "Steel"), (1.43 + 0.241 + 0.94 + 1.38, "Copper"), (0.241 + 0.69 + 1.38, "Aluminium")), //(0.7,Plastic)
