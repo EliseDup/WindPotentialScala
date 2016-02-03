a38283d3e43f2538b16557fe80233f2b4c0e0eba
EliseDup <Elise@wifi-secure1-742.sri.ucl.ac.be>
2016-02-03 09:48:19 +0100

    progress

--- src/main/scala/WindPotentialSimulation.scala
+++ src/main/scala/WindPotentialSimulation.scala
@@ -2,7 +2,7 @@
 
 import construction.Materials
 import landCover.GridData
-import squants.space.Degrees
+import squants.space._
 import operation.WindTurbine2MW
 import squants.space.SquareKilometers
 import operation.WindTurbineWithPower
@@ -14,13 +14,48 @@ import construction.WindTurbineComponents
 import operation.WindTurbineWithPower
 import squants.space.Kilometers
 import construction.Transmission
+import utils.Helper
+import java.io.File
+import java.io.PrintStream
+import java.io.FileOutputStream
 
 object WindPotentialSimulation {
   def main(args: Array[String]): Unit = {
+  plotCRUWind()
+    plotEuropeWind()
+    
+    
+    val cru = new GridData("results/CRU10min", Arcminutes(10), new WindTurbineWithPower("2MW"), new WindTurbineWithPower("5MWoffshore"), details = true)
+   
+    val ecmwf = new GridData("results/europe2002", Degrees(0.125), new WindTurbineWithPower("2MW"), new WindTurbineWithPower("5MWoffshore"))
+    val list = List((cru.windSpeeds(cru.landGrids(cru.clcGrids), atHub = true), "CRU"), (ecmwf.windSpeeds(ecmwf.landGrids(ecmwf.clcGrids), atHub = true), "ecmwf"))
+    PlotHelper.cumulativeDensity(list)
+PlotHelper.histogram(list(0)._1,title="CRU Data")
+PlotHelper.histogram(list(1)._1, title="EMCWF Data")
+    println("--- Hello ---")
+  }
 
-    val name = "world5years"
-    val wind = new GridData("results/" + name, Degrees(0.25), new WindTurbineWithPower("2MW"),  new WindTurbineWithPower("5MWoffshore"))
-    // wind.plotEROIVSCumulatedProduction()
+  def plot(wind: GridData) {
+    PlotHelper.cumulativeDensity(List((wind.windSpeeds(), "")), title = wind.name)
+    PlotHelper.cumulativeDensity(List((wind.powerDensities(), "")), title = wind.name)
+  }
+  def plotEuropeWind() {
+    val l = List("europe2002", "europeNovember2001", "europeFebruary2002", "europeMay2002", "europeAugust2002")
+    val speed = l.map({ w =>
+      val wind = new GridData("results/" + w, Degrees(0.125), new WindTurbineWithPower("2MW"), new WindTurbineWithPower("5MWoffshore"))
+      (wind.windSpeeds(wind.landGrids(wind.clcGrids)), w)
+    })
+    PlotHelper.cumulativeDensity(speed)
+  }
+
+  def plotCRUWind() {
+    val month = (0 until 12).toList
+    val w = new GridData("results/CRU10min", Arcminutes(10), new WindTurbineWithPower("2MW"), new WindTurbineWithPower("5MWoffshore"), details = true)
+      
+    val speeds = month.map(i => (w.windSpeedsMonth(i, w.landGrids(w.clcGrids)),i.toString))
+   
+    PlotHelper.cumulativeDensity(speeds)
+    //   PlotHelper.cumulativeDensity(densities)
 
   }
 }
--- src/main/scala/operation/WeibullDistribution.scala
+++ src/main/scala/operation/WeibullDistribution.scala
@@ -10,23 +10,24 @@ import construction.Materials
 object LoadHourCalculation {
   def main(args: Array[String]): Unit = {
     val turbine2MW = new WindTurbine2MW
-    val k = List(1.75,2.0,2.4)
+    val k = List(1.75, 2.0, 2.4)
     val v = (1 until 14).map(i => (0 until 10).map(_ * 0.1 + i)).flatten.toList
-    val euEstimation = v.map(i => Math.max(0, i*626.38-2003.3)).toList
-    val list = k.map(i =>{
+    val euEstimation = v.map(i => Math.max(0, i * 626.38 - 2003.3)).toList
+    val hoogrijkEstimation = v.map(i => Math.max(0, i * 565 - 1745)).toList
+    val list = k.map(i => {
       println(i)
-        (v, v.map(v => new Weibull(v, i).capacityFactor(turbine2MW)*365*24).toList, "K factor: "+i)
+      (v, v.map(v => new Weibull(v, i).capacityFactor(turbine2MW) * 365 * 24).toList, "K factor: " + i)
     })
-    PlotHelper.plotXY(list.toList :+ (v,euEstimation,"2 MW EU Report Estimation"), xLabel="Mean Wind Speed", yLabel="Load Hours", legend=true)
-   val error = (0 until v.size).map(i => Math.abs(euEstimation(i)-list(0)._2(i))/list(0)._2(i)).sum / v.size
+    PlotHelper.plotXY(list.toList :+ (v, euEstimation, "2 MW EU Report Estimation") :+ (v, hoogrijkEstimation, "Hoogrijg PHD Thesis"), xLabel = "Mean Wind Speed", yLabel = "Load Hours", legend = true)
+    val error = (0 until v.size).map(i => Math.abs(euEstimation(i) - list(0)._2(i)) / list(0)._2(i)).sum / v.size
     println("Mean Error :" + error)
-   
+
   }
 }
 
 class Weibull(meanspeed: Double, k: Double) {
   val wb = new WeibullDistribution(k, meanspeed)
-  val x = wb.sample(1000000).toList
+  val x = wb.sample(10000).toList
   def plot = PlotHelper.histogram(x, 100, "Weibull distribution scale :" + meanspeed + ", shape:" + k)
 
   def capacityFactor(turbine: WindTurbine): Double = x.map(v => turbine.power(MetersPerSecond(v))).foldLeft(Kilowatts(0))(_ + _) / x.size / turbine.ratedPower
