9e9e259f05e046189fb7480a84390f14bc729534
EliseDup <Elise@wifi-secure3-79.sri.ucl.ac.be>
2018-03-19 11:19:56 +0100

    Progress Solar Energy

---
+++ src/main/scala/solar_energy/SolarGrid.scala
@@ -0,0 +1,84 @@
+package solar_energy
+
+import squants.energy._
+import squants.time.Hours
+import squants.space._
+import squants.radio._
+import utils._
+import grid._
+import scala.io.Source
+import java.io.PrintStream
+
+object SolarGrid {
+  def _0_1deg = apply("../resources/data_solar/0_1deg_protected", Degrees(0.1))
+  def _0_5deg = apply("../resources/data_solar/0_5deg", Degrees(0.5))
+
+  def apply(name: String, res: Angle) = {
+    val list = Source.fromFile(name).getLines().toList
+    new SolarGrid(list.map(SolarCell(_, res)).toList)
+  }
+}
+
+class SolarGrid(val cells: List[SolarCell]) {
+  def country(c: String) = cells.filter(_.country.equalsIgnoreCase(c))
+
+  def write(logFile: String) {
+    val out_stream = new PrintStream(new java.io.FileOutputStream(logFile))
+
+    cells.map(c => out_stream.print(c.center.latitude.toDegrees + "\t" + c.center.longitude.toDegrees + "\t" +
+      c.distanceToCoast.toKilometers + "\n"))
+    out_stream.close()
+  }
+}
+
+class SolarCell(val center: GeoPoint, val resolution: Angle, val ghi: Irradiance, val dni: Irradiance,
+    val landCover: LandCoverType, val distanceToCoast: Length, val elevation: Length,
+    val slope_geq45: Double, val slope_geq0_5: Double,
+    val country: String, val protected_area: Boolean = false) {
+
+  val area = Helper.areaRectangle(center, resolution)
+  val suitableArea = if (protected_area) SquareKilometers(0) else landCover.solarFactor.mean * area * (1 - slope_geq45)
+  def area(lcType: LandCoverType): Area = if (lcType.equals(landCover)) area else SquareKilometers(0)
+
+  def powerDensity(tech: Technology) = 1.0 / tech.occupationRatio * (if (tech.directOnly) dni else ghi) * tech.efficiency * tech.performanceRatio
+
+  val pvPotential = suitableArea * powerDensity(PVTechnology)
+  val installedPV = suitableArea * PVTechnology.efficiency * WattsPerSquareMeter(1000) / PVTechnology.occupationRatio
+
+  val cspPotential = suitableArea * powerDensity(CSPTechnology)
+  val maxPotential = if (powerDensity(PVTechnology) > powerDensity(CSPTechnology)) pvPotential else cspPotential
+
+  def eroiPV: Double = {
+    if (ghi.value == 0 || suitableArea.value == 0) 0.0
+    else {
+      val out = Hours(25 * 365 * 24) * pvPotential
+      out / EmbodiedEnergyPV.inputs(installedPV, out)
+    }
+  }
+}
+
+object SolarCell {
+  def apply(line: String, resolution: Angle) = {
+    val i = line.split("\t")
+    new SolarCell(GeoPoint(Degrees(i(1).toDouble), Degrees(i(0).toDouble)), resolution,
+      WattsPerSquareMeter(i(2).toDouble / 24 * 1000), WattsPerSquareMeter(i(3).toDouble / 24 * 1000),
+      GlobCoverClasses.landCoverType(i(4).toDouble.toInt), Kilometers(i(5).toDouble), Meters(i(6).toDouble),
+      slope(i(7)), slope(i(8)),
+      i(9).toString, i(10).toInt == 1)
+  }
+  def slope(i: String) = {
+    val s = i.toDouble
+    if (s > 100) println("Slope > 100 !" + s)
+    math.max(0, s / 100.0)
+  }
+}
+
+object SolarUtils {
+  def areaList(list: List[SolarCell]) = list.map(_.area).foldLeft(SquareKilometers(0))(_ + _)
+  def suitableAreaList(list: List[SolarCell]) = list.map(_.suitableArea).foldLeft(SquareKilometers(0))(_ + _)
+}
+
+class Technology(val efficiency: Double, val performanceRatio: Double, val occupationRatio: Double, val directOnly: Boolean)
+
+object PVTechnology extends Technology(0.24, 0.883, 5, false)
+object CSPTechnology extends Technology(0.16, 1, 7.5, true)
--- src/main/scala/solar_energy/Test.scala
+++ src/main/scala/solar_energy/Test.scala
@@ -17,124 +17,76 @@ object Test {
   import PlotHelper._
   import SolarPotential._
   import SolarGrid._
+  import Helper._
   def main(args: Array[String]): Unit = {
-   
-    val grid = _0_5deg
-    val BE = grid.country("Belgium")
-    BE.map(i => println(i.ghi.toWattsPerSquareMeter*8760/1000))
-    //EROIPV(grid.cells)
-    // PlotHelper.histogram(potential)
-    println( grid.cells.map(c => c.slope_geq0_5 * c.area.toSquareKilometers).sum / 1E6)
-    println( grid.cells.map(c => c.slope_geq45 * c.area.toSquareKilometers).sum / 1E6)
-        println(area(grid.cells) / 1E6)
-    // printArea(grid)
-    def printArea(grid: SolarGrid) {
-      val list = grid.cells
-      println(list.size)
-      println("Total " + area(list).toSquareKilometers / 1E6)
-      println("Suitable " + list.map(_.suitableArea.toSquareKilometers).sum / 1E6)
-      println("Sparse " + list.map(g => g.area(SparseVegetation) + g.area(Grassland) + g.area(BareAreas)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
-      println("Forest " + list.map(g => g.area(Forests)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
-      println("Croplands " + list.map(g => g.area(CropLands)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
-      println("Shrubland " + list.map(g => g.area(Shrubland)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
-      println("MosaicVegetationCroplands " + list.map(g => g.area(MosaicVegetationCroplands)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
-      println("MosaicGrasslandForestShrubland" + list.map(g => g.area(MosaicGrasslandForestShrubland)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
-      println("Urban " + list.map(g => g.area(UrbanAreas)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
-      println("Flooded + Waters + Ice " + list.map(g => g.area(FloodedAreas) + g.area(WaterBodies) + g.area(Ice)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
-    }
 
-    def plotEROI(w: WorldGrid, g: List[GridCell]) {
+    val grid = _0_1deg
+    println(areaList(grid.cells.filter(_.protected_area)))
 
-      PlotHelper.cumulativeDensity(List((g.map(_.irradiance.month(0).toWattsPerSquareMeter), "January"), (g.map(_.irradiance.month(6).toWattsPerSquareMeter), "July")), legend = true,
-        xLabel = "% Sites", yLabel = "Monthly Irradiance [W/m²]")
-      w.writeGrid("jan_ju")
-
-      val eroiMax1 = Math.ceil(g.map(i => SolarPotential.eff_17.eroi(i, 1, true)).max).toInt
-      val eroiMax2 = Math.ceil(g.map(i => SolarPotential.eff_24.eroi(i, 1, true)).max).toInt
-
-      PlotHelper.plotXY(List(
-        SolarPotential.eff_17.potential_eroi((2 to 2 * eroiMax1).map(_ * 0.5).toList, true, g, "0.17"),
-        SolarPotential.eff_24.potential_eroi((2 to 2 * eroiMax2).map(_ * 0.5).toList, true, g, "0.24")),
-        xLabel = "Solar Potential [EJ/year]", yLabel = "EROI",
-        legend = true)
-
-    }
-    def EROIPV(g: List[SolarCell]) = {
-      val res = Helper.listValueVSCumulated(g.filter(g => g.pvPotential.value > 0 && g.eroiPV >= 1).map(g => (g.eroiPV, (g.pvPotential * Hours(365 * 24)).to(TerawattHours))))
-      PlotHelper.plotXY(List((res._1, res._2, "PV")), xLabel = "PV Potential [TWh/year]", yLabel = "EROI")
-      //  PlotHelper.cumulativeDensity(List( (g.map(c => (c.pvPotential *Hours(365*24)).to(TerawattHours)),"PV")), yLabel="PV Potential [TWh]")
-      //  PlotHelper.cumulativeDensity(g.map(c => (c.cspPotential *Hours(365*24)).to(TerawattHours)))
-    }
+    val countries = Helper.getLines("../countries_WWS2050", "\t").map(_(0))
+    val wp = 239.1751749
+    val efficiency = wp / 1000
+    countries.map(c => {
+      val x = grid.country(c)
+      //  println(x(0).ghi.toWattsPerSquareMeter)
+      println(c + "\t" + areaList(x).toSquareKilometers + "\t" +
+        x.map(_.suitableArea.toSquareKilometers).sum + "\t" +
+        x.map(_.installedPV.toMegawatts).sum + "\t" +
+        x.map(_.pvPotential.toMegawatts).sum + "\t"
+        + x.map(i => i.ghi.toWattsPerSquareMeter * efficiency / wp).sum / x.size)
+    })
   }
-}
-
-object SolarUtils {
-  def area(list: List[SolarCell]) = list.map(_.area).foldLeft(SquareKilometers(0))(_ + _)
-  def suitableArea(list: List[SolarCell]) = list.map(_.suitableArea).foldLeft(SquareKilometers(0))(_ + _)
-
-}
-object SolarGrid {
-  def _0_1deg = apply("../resources/data_solar/grid_0_1deg", Degrees(0.1))
-  def _0_5deg = apply("../resources/data_solar/grid_0_5deg", Degrees(0.5))
-  
-  def apply(name: String, res: Angle) = {
-    val list = Source.fromFile(name).getLines().toList
-    new SolarGrid(list.map(SolarCell(_, res)).toList)
+  def area_min_ghi(grid: SolarGrid, c: String) {
+    val country = grid.country(c)
+    if (country.size == 0) println(c + "\t" + "---")
+    else println(c + "\t" + areaList(country.filter(i => i.ghi.value * 24 >= 4000)).toSquareKilometers
+      + "\t" + areaList(country.filter(i => i.ghi.value * 24 >= 5000)).toSquareKilometers
+      + "\t" + areaList(country.filter(_.ghi.value * 24 >= 6000)).toSquareKilometers)
   }
 
-}
-class SolarGrid(val cells: List[SolarCell]) {
-  def country(c: String) = cells.filter(_.country.equalsIgnoreCase(c))
-
-  def write(logFile: String) {
-    val out_stream = new PrintStream(new java.io.FileOutputStream(logFile))
-
-    cells.map(c => out_stream.print(c.center.latitude.toDegrees + "\t" + c.center.longitude.toDegrees + "\t" +
-      c.powerDensity(PVTechnology).toWattsPerSquareMeter + "\t" +
-      c.powerDensity(CSPTechnology).toWattsPerSquareMeter + "\n"))
-    out_stream.close()
+  def printMeanGHI(c: String, grid: SolarGrid) {
+    val cells = grid.country(c)
+    println(c + "\t" + cells.map(_.ghi.toWattsPerSquareMeter).sum / cells.size)
   }
-}
+  def printArea(list: List[SolarCell]) {
 
-class SolarCell(val center: GeoPoint, val resolution: Angle, val ghi: Irradiance, val dni: Irradiance,
-    val landCover: LandCoverType, val distanceToCoast: Length, val elevation: Length,
-    val slope_geq45 : Double, val slope_geq0_5 : Double,
-    val country: String, val protected_area: Boolean = false) {
+    println(list.size)
+    println("Total " + areaList(list).toSquareKilometers / 1E6)
+    println("Slope > 45 " + list.map(i => i.area.toSquareKilometers * i.slope_geq45).sum / 1E6)
+    println("Slope > 0.5 " + list.map(i => i.area.toSquareKilometers * i.slope_geq0_5).sum / 1E6)
 
-  val area = Helper.areaRectangle(center, resolution)
-  val suitableArea = landCover.solarFactor.mean * area //* (1 - slope_geq45)
-  def area(lcType: LandCoverType): Area = if (lcType.equals(landCover)) area else SquareKilometers(0)
-
-  def powerDensity(tech: Technology) = 1.0 / tech.occupationRatio * (if (tech.directOnly) dni else ghi) * tech.efficiency * tech.performanceRatio
-
-  val pvPotential = suitableArea * powerDensity(PVTechnology)
-  val installedPV = suitableArea * PVTechnology.efficiency * WattsPerSquareMeter(1000) / PVTechnology.occupationRatio
-
-  val cspPotential = suitableArea * powerDensity(CSPTechnology)
-  val maxPotential = if (powerDensity(PVTechnology) > powerDensity(CSPTechnology)) pvPotential else cspPotential
-
-  def eroiPV: Double = {
-    if (ghi.value == 0 || suitableArea.value == 0) 0.0
-    else {
-      val out = Hours(25 * 365 * 24) * pvPotential
-      out / EmbodiedEnergyPV.inputs(installedPV, out)
-    }
+    println("Suitable " + list.map(_.suitableArea.toSquareKilometers).sum / 1E6)
+    println("Sparse " + list.map(g => g.area(SparseVegetation) + g.area(Grassland) + g.area(BareAreas)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
+    println("Forest " + list.map(g => g.area(Forests)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
+    println("Croplands " + list.map(g => g.area(CropLands)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
+    println("Shrubland " + list.map(g => g.area(Shrubland)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
+    println("MosaicVegetationCroplands " + list.map(g => g.area(MosaicVegetationCroplands)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
+    println("MosaicGrasslandForestShrubland" + list.map(g => g.area(MosaicGrasslandForestShrubland)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
+    println("Urban " + list.map(g => g.area(UrbanAreas)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
+    println("Flooded + Waters + Ice " + list.map(g => g.area(FloodedAreas) + g.area(WaterBodies) + g.area(Ice)).foldLeft(SquareKilometers(0))(_ + _).toSquareKilometers / 1E6)
   }
-}
 
-object SolarCell {
-  def apply(line: String, resolution: Angle) = {
-    val i = line.split("\t")
-    new SolarCell(GeoPoint(Degrees(i(1).toDouble), Degrees(i(0).toDouble)), resolution,
-      WattsPerSquareMeter(i(2).toDouble / 24 * 1000), WattsPerSquareMeter(i(3).toDouble / 24 * 1000),
-      GlobCoverClasses.landCoverType(i(4).toDouble.toInt), Kilometers(i(5).toDouble), Meters(i(6).toDouble),
-      math.max(0, i(7).toDouble / 100.0), math.max(0, i(8).toDouble/100.0),
-      i(9).toString, false)
-    // if (i.size >= 10) i(9).toInt == 1 else false)
+  def plotEROI(w: WorldGrid, g: List[GridCell]) {
+
+    PlotHelper.cumulativeDensity(List((g.map(_.irradiance.month(0).toWattsPerSquareMeter), "January"), (g.map(_.irradiance.month(6).toWattsPerSquareMeter), "July")), legend = true,
+      xLabel = "% Sites", yLabel = "Monthly Irradiance [W/m²]")
+    w.writeGrid("jan_ju")
+
+    val eroiMax1 = Math.ceil(g.map(i => SolarPotential.eff_17.eroi(i, 1, true)).max).toInt
+    val eroiMax2 = Math.ceil(g.map(i => SolarPotential.eff_24.eroi(i, 1, true)).max).toInt
+
+    PlotHelper.plotXY(List(
+      SolarPotential.eff_17.potential_eroi((2 to 2 * eroiMax1).map(_ * 0.5).toList, true, g, "0.17"),
+      SolarPotential.eff_24.potential_eroi((2 to 2 * eroiMax2).map(_ * 0.5).toList, true, g, "0.24")),
+      xLabel = "Solar Potential [EJ/year]", yLabel = "EROI",
+      legend = true)
+
   }
+  def EROIPV(g: List[SolarCell]) = {
+    val res = Helper.listValueVSCumulated(g.filter(g => g.pvPotential.value > 0 && g.eroiPV >= 1).map(g => (g.eroiPV, (g.pvPotential * Hours(365 * 24)).to(TerawattHours))))
+    PlotHelper.plotXY(List((res._1, res._2, "PV")), xLabel = "PV Potential [TWh/year]", yLabel = "EROI")
+    //  PlotHelper.cumulativeDensity(List( (g.map(c => (c.pvPotential *Hours(365*24)).to(TerawattHours)),"PV")), yLabel="PV Potential [TWh]")
+    //  PlotHelper.cumulativeDensity(g.map(c => (c.cspPotential *Hours(365*24)).to(TerawattHours)))
+  }
+
 }
-
-class Technology(val efficiency: Double, val performanceRatio: Double, val occupationRatio: Double, val directOnly: Boolean)
-
-object PVTechnology extends Technology(0.17, 0.81, 5, false)
-object CSPTechnology extends Technology(0.16, 1, 7.5, true)
