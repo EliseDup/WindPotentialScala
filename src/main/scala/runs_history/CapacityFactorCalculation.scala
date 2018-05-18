3690875ff1eab60bb83389afd52033da6bca2e37
EliseDup <Elise@mac-1o0-339.local>
2018-05-18 11:04:34 +0200

    Progress (simulate hourly csp production)

---
+++ src/main/scala/runs_history/CapacityFactorCalculation.scala
@@ -0,0 +1,67 @@
+package runs_history
+
+import grid.WorldGrid
+import squants.space.Degrees
+import wind_energy.WindPotential
+import squants.radio.WattsPerSquareMeter
+import utils.TerawattHours
+import squants.space.SquareKilometers
+import utils.Terawatts
+import grid.GridCell
+import squants.energy.Gigawatts
+import squants.radio.Irradiance
+import squants.time.Hours
+
+object CapacityFactorCalculation {
+  import utils.Helper._
+  def main(args: Array[String]): Unit = {
+    val wp = WindPotential()
+    val countries = getLines("countries_students").map(_(0))
+    val eroi_min = (2 until 40).map(_ * 0.5).toList
+    val wind = new WorldGrid("runs_history/wind_2017/results_wind_2017", Degrees(0.75), eroi_min, 34, 151, true, false)
+    val d = WattsPerSquareMeter(2)
+    pr("Country"); pr("EROI Minimum"); pr("Onshore Production Potential [TWh/year]"); pr("Onshore Capacity Potential [GW]"); pr("Onshore Mean Installed Capacity Density [MW/km2]"); pr("Onshore Mean Capacity Factor [%]");
+    pr("Offshore Production Potential [TWh/year]"); pr("Offshore Capacity Potential [GW]"); pr("Offshore Mean Installed Capacity Density [MW/km2]"); pr("Ofshore Mean Capacity Factor [%]");
+    println()
+    for(eroi  <- (1 until 15)){
+ 
+    for (c <- countries) {
+      print(eroi, c)
+    }}
+    def print(eroi: Double, c: String) {
+      val cells = wind.country(c);
+      val on = cells.filter(_.onshore); val off = cells.filter(_.offshore);
+      val pot_on = wp.potential(eroi, true, on).to(TerawattHours)
+      val cap_on = wp.installedCapacity(eroi, true, on).to(Gigawatts)
+      val cd_on = wp.meanCapacityDensity(on, eroi)
+      val pot_off = wp.potential(eroi, true, off).to(TerawattHours)
+      val cap_off = wp.installedCapacity(eroi, true, off).to(Gigawatts)
+      val cd_off = wp.meanCapacityDensity(off, eroi)
+
+      pr(c); pr(eroi); pr(pot_on); pr(cap_on); pr(cd_on); pr(if(cap_on > 0) (pot_on / (cap_on * 8.76) * 100) else 0.0)
+      pr(pot_off); pr(cap_off); pr(cd_off); pr(if(cap_off > 0) (pot_off / (cap_off * 8.76) * 100) else 0.0)
+      println()
+
+    }
+    def printFixed(eroi: Double, c: String, cd: Irradiance) {
+      val cells = wind.country(c);
+      val on = cells.filter(_.onshore); val off = cells.filter(_.offshore);
+      val pot_on = wp.potentialFixedDensity(cd, eroi, on, true)
+      val cap_on = cd * (on.map(c => if (wp.eroi(c, cd, true) >= eroi) c.suitableArea(wp) else SquareKilometers(0)).foldLeft(SquareKilometers(0))(_ + _))
+      val cf_on = pot_on / (cap_on * Hours(365 * 24))
+
+      val pot_off = wp.potentialFixedDensity(cd, eroi, off, true)
+      val cap_off = cd * (off.map(c => if (wp.eroi(c, cd, true) >= eroi) c.suitableArea(wp) else SquareKilometers(0)).foldLeft(SquareKilometers(0))(_ + _))
+      val cf_off = pot_off / (cap_off * Hours(365 * 24))
+
+      pr(c);
+      pr(pot_on.to(TerawattHours)); pr(cap_on.to(Gigawatts)); pr(cd.toWattsPerSquareMeter); pr(cf_on * 100)
+      pr(pot_off.to(TerawattHours)); pr(cap_off.to(Gigawatts)); pr(cd.toWattsPerSquareMeter); pr(cf_off * 100)
+      println()
+
+    }
+  }
+
+  def pr(c: String) = print(c + "\t")
+  def pr(c: Double) = print(c + "\t")
+}
---
+++ src/main/scala/solar_energy/SolarHourlySimulation.scala
@@ -0,0 +1,69 @@
+package solar_energy
+
+import squants.space._
+import squants.radio._
+import squants.energy._
+import squants.time.Hours
+import utils._
+import java.io.PrintStream
+object SolarHourlySimulation {
+  import SolarPower._
+  import Helper._
+  import DayMonth._
+  
+  def main(args: Array[String]): Unit = {
+    val maxDay = 365
+    val d = (1 to maxDay).toList
+    val h = (1 to 24).toList
+
+    val solar = (getLines("../resources/cru_data/cru_solarByMonth").map(i => {
+      (GeoPoint(Degrees(i(0).toDouble), Degrees(i(1).toDouble)), (0 to 12).toList.map(j => WattsPerSquareMeter(i(j + 2).toDouble)))
+    })).filter(i => !i._2.exists(j => j.value == 0))
+    println("Data loaded" + "\t" + solar.size)
+  val out_stream = new PrintStream(new java.io.FileOutputStream("test"))
+
+    val test = solar.filter(i => math.abs(i._1.latitude.value)<=45)
+    test.map(t => {
+      val res = simulate(t,2,0)
+      out_stream.print(t._1.latitude.value +"\t" + t._1.longitude.value +"\t" + t._2(0).value + "\t" + res._1 + "\t" + res._2 +"\n")
+    }
+  )
+  out_stream.close()
+  }
+  def simulate(x : (GeoPoint, List[Irradiance]), sm: Double, hours_storage: Int) :(Double,Double) = simulate(x._1.latitude, x._2(0), (1 to 12).toList.map(x._2(_)), sm, hours_storage)
+  def simulate(latitude: Angle, irradiance_year: Irradiance, irradiance_month: List[Irradiance], sm: Double, hours_storage: Int):(Double,Double) = {
+    val maxDay = 365
+    val d = (1 to maxDay).toList; val h = (1 to 24).toList;
+    val dailyRad = d.map(day => dailyDirectRadiation(latitude, irradiance_month(month_dayInMonth(day)._1), day))
+    val hourlyRad = d.map(day => h.map(hour => hourlyDirectRadiation(latitude, irradiance_month(month_dayInMonth(day)._1), day, hour))).flatten
+
+    val power = Megawatts(1); val cap = power * Hours(1);
+    val area = power * sm / (WattsPerSquareMeter(950) * 0.15)
+    val storageSize = MegawattHours(hours_storage)
+    var storage = storageSize / 2; var totalProd = MegawattHours(0)
+
+    val prod: List[(Energy, Energy, Energy, Energy)] = hourlyRad.map(i => {
+      val e = area * i * 0.15 * Hours(1)
+      if (e > cap) {
+        val sup = e - cap
+        val e_stored = if (storageSize - storage > sup) sup else WattHours(Math.max((storageSize - storage - sup).value, 0))
+        val curt = sup - e_stored
+        storage += e_stored
+        totalProd += cap
+        (cap, e_stored, storage, -curt)
+      } // Enough storage to ensure 1 MWH
+      else if (storage > cap - e) {
+        storage -= cap - e
+        totalProd += cap
+        (cap, MegawattHours(0), storage, WattHours(0))
+      } else {
+        val p = e + storage
+        storage = MegawattHours(0)
+        totalProd += p
+        (p, MegawattHours(0), storage, WattHours(0))
+      }
+    })
+    (totalProd / (Megawatts(1) * Hours(365 * 24)), area * irradiance_year * 0.15 * Hours(365 * 24) / (Megawatts(1) * Hours(365 * 24)))
+  }
+
+}
